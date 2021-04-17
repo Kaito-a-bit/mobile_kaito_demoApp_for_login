58de2032e94f28e69ffa0df7f568708ace1a7865
Kaito Arai <arakai0128@gmail.com>
2021-04-17 13:27:09 +0900

    implemented the first url and webview for authorization

---
+++ DemoAppForLogin/DemoAppForLogin/LogInWebPageViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LogInWebPageViewController.swift
+//  DemoAppForLogin
+//
+//  Created by 荒井海斗 on 2021/04/17.
+//
+
+import UIKit
+import WebKit
+
+class LogInWebPageViewController: UIViewController {
+
+    var inheritedUrl: String!
+    var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let webConfig = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfig)
+        view = webView
+        
+        let url = URL(string: inheritedUrl)
+        if let unwrapURL = url {
+            let qiitaRequest = URLRequest(url: unwrapURL)
+            webView.load(qiitaRequest)
+        }
+        
+    }
+    
+
+
+}
---
+++ DemoAppForLogin/DemoAppForLogin/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  DemoAppForLogin
+//
+//  Created by 荒井海斗 on 2021/04/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var urlToInherit: String!
+    
+    @IBAction func loginButton(_ sender: Any) {
+        performSegue(withIdentifier: "FromLoginPageToWebPage", sender: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "FromLoginPageToWebPage" {
+            let next = segue.destination as! LogInWebPageViewController
+            urlToInherit = "https://qiita.com/api/v2/oauth/authorize?client_id=5aa950691c4276bf436ad317638741e1e7a98955"
+            next.inheritedUrl = self.urlToInherit
+        }
+    }
+
+
+}
+
